0ea7c8782429fb0f1a764d63a1591e9621dd7116
Andrea04172004 <kachmar24@ukr.net>
2022-02-04 16:18:11 +0200

    first lab

---
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/binaryFileProgram.cpp.obj"
+  "CMakeFiles/lab1.dir/binaryFileProgram.cpp.obj.d"
+  "CMakeFiles/lab1.dir/classes/Patient.cpp.obj"
+  "CMakeFiles/lab1.dir/classes/Patient.cpp.obj.d"
+  "CMakeFiles/lab1.dir/main.cpp.obj"
+  "CMakeFiles/lab1.dir/main.cpp.obj.d"
+  "CMakeFiles/lab1.dir/textFileProgram.cpp.obj"
+  "CMakeFiles/lab1.dir/textFileProgram.cpp.obj.d"
+  "lab1.exe"
+  "lab1.exe.manifest"
+  "lab1.pdb"
+  "liblab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
