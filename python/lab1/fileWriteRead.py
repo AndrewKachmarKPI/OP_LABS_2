bae0ca2157b8a322191f68457d2491c0a0ab92f0
Andrea04172004 <kachmar24@ukr.net>
2022-02-06 13:09:43 +0200

    sad

--- python/lab1/fileWriteRead.py
+++ python/lab1/fileWriteRead.py
@@ -1,33 +1,44 @@
+import sys
+
 def createTextFile():
     print("Enter text")
-    with open("startText.txt", "w") as file:
-     while True:
-         line = input()
-         if line:
-             file.write(line + "\n")
-         else:
-             break
+    with open("startText.txt", "w") as file:  # Створення та відкриття файлу для запису
+        while True:  # Цикл для введення рядків
+            line = input()  # Введення нового рядка
+            if line:  # Перевірка чи введений рядок не пустий
+                file.write(line + "\n")  # Запис значення нового рядка у файл
+            else:
+                break  # Вихід з циклу закінчення вводу даних
+    file.close()  # Закриття файлу
+
+def printFileContent(fileName):
+    try:
+        file = open(fileName, 'r')  # Відкриття файлу для считування
+    except OSError:
+        print("File open error!")  # Виведення повідомляння про помилку відкривання файла
+        sys.exit()  # Зупинка програми
+    print(file.read())  # Виведення вмісту файла
     file.close()
 
-def printFileText(fileName):
-    for line in readFile(fileName):
-        print(line)
-
 def appendLineLength():
-    with open("finalText.txt", "r+") as file:
-        for line in file.readlines():
-            file.write(line+"("+str(len(line))+")")
-
-
-
+    lines = readFile("finalText.txt") #Зчитування рядків файлу
+    with open("finalText.txt", "w") as file: #Відкриття файлу для запису
+        for line in lines:  #Цикл по усіх рядках
+            line = line.replace("\n", "")  #Видалення пробілів
+            line += "(" + str(len(line)) + ")" #Дописування довжини рядка
+            file.write(line + "\n")  #Запис у файл
+    file.close() #Закриття файла
 
 def sortLinesAndWriteToFile(lines):
-    sortedLines = sorted(lines, key=len)
-    with open("finalText.txt", "w") as file:
-        for line in sortedLines:
-            file.write(line)
-
+    sortedLines = sorted(lines, key=len) #Сортування рядків по довжині
+    with open("finalText.txt", "w") as file: #Відкриття файлу для запису
+        for line in sortedLines: #Цикл по рядках
+            file.write(str(line)) #Запис відсортованого рядка
 
 def readFile(fileName):
-    with open(fileName, "r") as file:
-        return file.readlines()
+    try:
+        file = open(fileName, 'r')  # Відкриття файлу для считування
+    except OSError:
+        print("File open error!")  # Виведення повідомляння про помилку відкривання файла
+        sys.exit()  # Зупинка програми
+    return file.readlines()  # Поверняння рядків
--- python/lab1/main.py
+++ python/lab1/main.py
@@ -1,14 +1,14 @@
 from fileWriteRead import *
 
-
-
 if __name__ == '__main__':
     createTextFile()
     lines = readFile("startText.txt")
     sortLinesAndWriteToFile(lines)
     appendLineLength()
-    print("Start Text")
-    printFileText("startText.txt")
-    print("Final Text")
-    printFileText("finalText.txt")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    print("Start file")
+    printFileContent("startText.txt")
+    print("Final file")
+    printFileContent("finalText.txt")
+
+
