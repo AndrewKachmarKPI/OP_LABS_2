705925b55ecc33cb9028f2d8467a8cde7361326d
Andrea04172004 <kachmar24@ukr.net>
2022-04-01 20:40:29 +0300

    third lab

--- python/lab3/LineService.py
+++ python/lab3/LineService.py
@@ -1,19 +1,16 @@
 from random import randrange
-
 from Line import Line
 
-
-def generateLinesManual():  # Генерування масиву ліній
+def generateLinesManual():  # Генерування масиву прямих
     lines = []  # Створення масиву ліній
-    for i in range(0, int(input('Input number of lines:'))):  # Цикл по кількості ліній
+    for i in range(0, int(input('Input number of lines:'))):
         lines.append(Line(int(input('Input parameter [a]:')),  # Створення та ініціалізація обєкту
                           int(input('Input parameter [b]:')),
                           int(input('Input parameter [c]:'))))
     return lines
 
-
-def generateLinesRandom():  # Генерування масиву ліній
+def generateLinesRandom():  # Генерування масиву прямих
     lines = []  # Створення масиву ліній
     for i in range(0, int(input('Input number of lines:'))):  # Цикл по кількості ліній
-        lines.append(Line(randrange(40), randrange(40), (randrange(40))))  # Створення та ініціалізація обєкту
+        lines.append(Line(randrange(50), randrange(200), randrange(100)))  # Створення та ініціалізація обєкту
     return lines
--- python/lab3/main.py
+++ python/lab3/main.py
@@ -3,11 +3,11 @@ from LineService import *
 if __name__ == '__main__':
     xPos = int(input("Enter x pos:"))  # Введення координати x
     yPos = int(input("Enter y pos:"))  # Введення координати y
-    lines = generateLinesRandom()  # Генерування масиву обєктів Лінії
+    lines = generateLinesRandom()  # Генерування масиву обєктів
     linesIndexes = []  # Створення масиву індексів
-    for i in range(0, len(lines)):  # Цикл по усіх створених лініях
-        if lines[i].isLineContainsPoint(xPos, yPos):  # Перевірка чи лінія містить точку
-            linesIndexes.append(i)  # Додавання індексу знайденої лінії
+    for i in range(0, len(lines)):  # Цикл по усіх створених прямих
+        if lines[i].isLineContainsPoint(xPos, yPos):  # Перевірка чи пряма містить точку
+            linesIndexes.append(i)  # Додавання індексу знайденої прямої
     print("Lines with indexes:" + str(linesIndexes) + " contains -> " + "Point (" + str(xPos) + "," + str(yPos) + ")")
     for i in linesIndexes:
-        print(lines[i].printLine())
+        lines[i].printLine()  # Виведення знайдених прямих
