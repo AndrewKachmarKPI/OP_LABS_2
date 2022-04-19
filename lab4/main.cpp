bf3d3245510c8d711106d3cf4d55ff9fa140c8c7
Andrea04172004 <kachmar24@ukr.net>
2022-04-19 13:47:58 +0300

    lab 4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,12 +4,19 @@
 using namespace std;
 
 int main() {
-    double v1_x, v1_y,v2_x, v2_y = 0;
-    cout << "Input v1 x=";cin>>v1_x;
-    cout << "Input v1 y=";cin>>v1_y;
-    cout << "Input v2 x=";cin>>v2_x;
-    cout << "Input v2 y=";cin>>v2_y;
-    AreaVector v1 = (AreaVector(v1_x, v1_y)) * 2;
+    double v1_x, v1_y, v2_x, v2_y = 0;
+    cout << "Input v1 x=";
+    cin >> v1_x;
+    cout << "Input v1 y=";
+    cin >> v1_y;
+    cout << "Input v2 x=";
+    cin >> v2_x;
+    cout << "Input v2 y=";
+    cin >> v2_y;
+    AreaVector v1 = AreaVector(v1_x, v1_y);
+    cout << "Vector V1 old length:" << v1.getLength() << endl;
+    v1 = v1 * 2;
+    cout << "Vector V1 new length:" << v1.getLength() << endl;
     AreaVector v2 = AreaVector();
     v2.setXPos(v2_x);
     v2.setYPos(v2_y);
